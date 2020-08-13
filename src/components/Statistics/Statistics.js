50d49c257386920c486bf9fe3586445abca19ab7
Andrii Makohin <andreas2010@ukr.net>
2020-08-13 19:44:59 +0300

    fix style

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,9 +5,11 @@ import Styles from "./Statistics.module.css";
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <>
+      <div className = {Styles.status}>
       <p className={Styles.good}>Good:{good}</p>
       <p className={Styles.neutral}>Neutral:{neutral}</p>
       <p className={Styles.bad}>Bad:{bad}</p>
+      </div>
       <p className={Styles.items}>Total:{total}</p>
       <p className={Styles.positive}>
         Positive feedback: {positivePercentage}%
