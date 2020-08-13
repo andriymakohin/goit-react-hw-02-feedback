5ce2821429da5fd22b3702d71d312219b05651da
Andrii Makohin <andreas2010@ukr.net>
2020-08-13 19:16:06 +0300

    Updates

---
+++ precache-manifest.0253fbe9b3156e7fa50a15b9a4541a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e88566da27914f3bbb7fe882fe7f40",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1f4f9ee00c639adcb6fa",
+    "url": "/goit-react-hw-02-feedback/static/css/main.9e1a22c0.chunk.css"
+  },
+  {
+    "revision": "7cc1c08804622ec80579",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a585c93f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a585c93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4f9ee00c639adcb6fa",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f90781c5.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
